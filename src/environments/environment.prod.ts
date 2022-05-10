5f100a7f73ec793730d50d93c5e0261111031fd0
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2022-05-10 13:40:09 -0400

    update env FE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  STRIPE_PUBLISHABLE_KEY: 'pk_test_51Ksf5xLnWcuSTW5Te8X53ZzzlrX8pxVnP3oA7E0liOma9p9XRkcINUr8pW3hDvFHC4hyvdxEDmNQp80ruqjjzFsr00uthGcnR7',
+  STRIPE_PUBLISHABLE_KEY: 'pk_live_51Ksf5xLnWcuSTW5TBWzeT8BhCPPMiRh30gUQzfW3WdvvWCkAcEp8Our2oGvGpzAVhoRoKSHEMujDs6fqzDXa2jHS005NJWaSeF',
   contentfulVolidaEvents: {
     spaceId: 'dt4p1vme6bp9',
     token: 'GmuP75B48SiWSBu9fY6Nw9l826e8NYwjc2x0jllo9qw',
