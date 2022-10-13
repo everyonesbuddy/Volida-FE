b2e4b92aa789d173b757ada70272be1ac381a869
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2022-10-12 23:10:32 -0400

    some changes to UI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,25 +1,26 @@
 export const environment = {
   production: true,
-  STRIPE_PUBLISHABLE_KEY: 'pk_live_51Ksf5xLnWcuSTW5TBWzeT8BhCPPMiRh30gUQzfW3WdvvWCkAcEp8Our2oGvGpzAVhoRoKSHEMujDs6fqzDXa2jHS005NJWaSeF',
+  STRIPE_PUBLISHABLE_KEY:
+    'pk_live_51Ksf5xLnWcuSTW5TBWzeT8BhCPPMiRh30gUQzfW3WdvvWCkAcEp8Our2oGvGpzAVhoRoKSHEMujDs6fqzDXa2jHS005NJWaSeF',
   contentfulVolidaEvents: {
     spaceId: 'dt4p1vme6bp9',
     token: 'GmuP75B48SiWSBu9fY6Nw9l826e8NYwjc2x0jllo9qw',
     contentTypeIds: {
-     product: 'volidaEvents',
-   },
+      product: 'volidaEvents',
+    },
   },
   contentfulVolidaRewatchEvents: {
     spaceId: 'dt4p1vme6bp9',
     token: 'OezF74wr2ytWNKCJLFczt8ZcvzqkLIMQzDgzswiIxNw',
     contentTypeIds: {
-     product: 'volidaComedySpecials',
-   },
+      product: 'volidaComedySpecials',
+    },
   },
   contentfulVolidaFeaturedActs: {
     spaceId: 'dt4p1vme6bp9',
     token: 'w_bDMvADRPGSg-yWXVjfEwQUaYwjtaGE0X_JqZTs1pw',
     contentTypeIds: {
-     product: 'volidaFeaturedActs',
-   },
-  }
+      product: 'volidaFeaturedActs',
+    },
+  },
 };
