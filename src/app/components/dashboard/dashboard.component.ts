b410fcd40aab1c3d222149e0ee016f407b3866b4
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2022-10-14 20:57:30 -0400

    clean dashboard component of dead files

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,49 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Entry } from 'contentful'
-import { RecentStreamService } from 'src/app/services/recent-stream.service';
+import { Entry } from 'contentful';
 import { EventsService } from 'src/app/services/events.service';
 import { SubscriptionService } from 'src/app/services/subscription.service';
-import { FeaturedActsService } from 'src/app/services/featured-acts.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  events: Entry<any> [] = [];
-  recentStreams: Entry<any> [] = [];
-  featuredActs: Entry<any> [] = [];
+  events: Entry<any>[] = [];
+  recentStreams: Entry<any>[] = [];
+  featuredActs: Entry<any>[] = [];
 
-  constructor(private eventService: EventsService, private recentStreamService: RecentStreamService, private featuredActsService: FeaturedActsService, private router: Router, private subscriptionService: SubscriptionService) { }
+  constructor(
+    private eventService: EventsService,
+    private router: Router,
+    private subscriptionService: SubscriptionService
+  ) {}
 
   ngOnInit(): void {
     //get all events
-    this.eventService.getAllEvents()
-      .then(events => {
-        this.events = events;
-      });
-
-      // get all On Demand And Recent Live Streams
-      this.recentStreamService.getAllRecentLiveStreams()
-        .then(recentStreams => {
-          this.recentStreams = recentStreams
-        })
-
-        // get all Featured Act
-      this.featuredActsService.getAllFeaturedActs()
-      .then(featuredActs => {
-        this.featuredActs = featuredActs
-      })
+    this.eventService.getAllEvents().then((events) => {
+      this.events = events;
+    });
   }
 
-  detailPage(id:any){
-    this.router.navigate(['/detailPage/'+id]);
+  detailPage(id: any) {
+    this.router.navigate(['/detailPage/' + id]);
   }
-
-  recentStreamDetailPage(id: any) {
-    this.router.navigate(['recentStreamPage/'+id]);
-  }
-
 }
