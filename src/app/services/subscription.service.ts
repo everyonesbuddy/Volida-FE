60b8b7c8f5c422b144bb64ee58ceeaecb9971fb8
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2023-01-15 13:55:46 -0500

    remove bbt 23

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -38,7 +38,7 @@ export class SubscriptionService {
         if (response.subscriptions.length === 0) {
           this.router.navigate(['']);
         } else {
-          this.router.navigate(['/processed']);
+          this.router.navigate(['/events']);
         }
       });
   }
